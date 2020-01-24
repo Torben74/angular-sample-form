d68736f6b56993942e6ec3634776c4f59d5d6a39
Torben74 <torben.thurau@googlemail.com>
2020-01-24 22:43:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sfe2qb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sfe2qb)
---
+++ src/app/sample-form/sample-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-sample-form',
+  templateUrl: './sample-form.component.html',
+  styleUrls: ['./sample-form.component.css']
+})
+export class SampleFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  todoForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+  });
+
+  onSubmit() {
+  // TODO: Use EventEmitter with form value
+  console.warn(this.todoForm.value);
+}
+}
